47c0ba4b5e66ef417f0da84d40279214aff9ba71
GabrielAranhaMello2007 <78978047+GabrielAranhaMello2007@users.noreply.github.com>
2021-02-12 15:22:17 -0300

    Create Banco_de_dados.py
    Banco de dados ( Faz parte do código "Login_Cadastro0")

---
+++ Banco_de_dados.py
@@ -0,0 +1,7 @@
+# Para executaro código renomeie este arquivo como "Banco_de_dados"
+users = {}
+
+# Exemplo de como ficaria cadastrado o usário
+login = "GabrielAranhaMello"
+senha = "12345$%"
+users.update({login: senha})
