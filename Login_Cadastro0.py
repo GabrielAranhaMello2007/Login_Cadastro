005b2672f7ca7761ab4dd3272baa81f24cd2ccd2
GabrielAranhaMello2007 <78978047+GabrielAranhaMello2007@users.noreply.github.com>
2021-02-19 18:39:37 -0300

    Update Login_Cadastro0.py

--- Login_Cadastro0.py
+++ Login_Cadastro0.py
@@ -7,90 +7,152 @@ import PySimpleGUI as sg
 from Banco_de_dados import *
 
 
+import PySimpleGUI as Sg
+from Banco_de_dados import *
+import time
+
+
 def janela_login():
-    """Retorna a página de login"""
-    sg.theme('Reddit')
+
+    Sg.theme('DarkBlack')
     layout = [
-        [sg.Text('Login'), sg.Input(key='login')],
-        [sg.Text('Senha'), sg.Input(password_char='*', key='senha')],
-        [sg.Button('Cadastro')],
-        [sg.Button('Entrar')]
+        [Sg.Text('Login', size=(30, 1))],
+        [Sg.Input(key='login', size=(25, 1))],
+        [Sg.Text('', text_color='red', size=(25, 1), key='text1')],
+        [Sg.Text('Senha', size=(30, 1))],
+        [Sg.Input(size=(25, 1), key='password', password_char='*')],
+        [Sg.Text('', text_color='red', key='text2', size=(30, 1))],
+        [Sg.Text(size=(29, 10))],
+        [Sg.Button('Entrar')],
+        [Sg.Button('Cadastro')],
+        [Sg.Text(size=(0, 1))]
     ]
-    return sg.Window('janela_login', layout=layout, finalize=True)
+    return Sg.Window('Janela Login', finalize=True, layout=layout)
+
+
+def janela_login_correct():
+
+    Sg.theme('DarkBlack')
+    layout = [
+        [Sg.Text(size=(32, 1))],
+        [Sg.Text(size=(5, 0)), Sg.Text('Acesso concedido\n'
+                                       'A janela será encerrada em ...5 segundos', text_color='green', key='jlc')],
+        [Sg.Text()]
+    ]
+    return Sg.Window('Acesso concedido', finalize=True, layout=layout)
+
+
+def janela_cadastro_correct():
+
+    Sg.theme('DarkBlack')
+    layout = [
+        [Sg.Text(size=(32, 1))],
+        [Sg.Text(size=(5, 1)), Sg.Text('Conta criada com sucesso', text_color='green', key='jcc')],
+        [Sg.Text()]
+    ]
+    return Sg.Window('Acesso concedido', finalize=True, layout=layout)
 
 
 def janela_cadastro():
-    """Retorna a página de cadastro"""
-    sg.theme('Reddit')
-    layout_cadastro = [
-        [sg.Text('Seu novo login'), sg.Input(key='login')],
-        [sg.Text('Sua nova senha'), sg.Input(password_char='*', key='senha')],
-        [sg.Button('< Voltar', key='voltar')],
-        [sg.Button('Criar nova conta', key='cnv')]
+
+    Sg.theme('DarkBlack')
+    layout = [
+        [Sg.Text('Novo login', size=(30, 1))],
+        [Sg.Input('', key='login_cadastro', size=(25, 1))],
+        [Sg.Text('', key='text3', text_color='red', size=(25, 1))],
+        [Sg.Text('Crie sua senha', size=(30, 1))],
+        [Sg.Input(key='password_cadastro_1', size=(25, 1), password_char='*')],
+        [Sg.Text('Confirme sua senha', size=(30, 1))],
+        [Sg.Input(key='password_cadastro_2', size=(25, 1), password_char='*')],
+        [Sg.Text('', key='text4', text_color='red', size=(30, 1))],
+        [Sg.Text(size=(29, 10))],
+        [Sg.Button('Cadastrar')],
+        [Sg.Text(size=(0, 1))]
     ]
-    return sg.Window('janela_cadastro', layout=layout_cadastro, finalize=True)
+    return Sg.Window('Janela Cadastro', finalize=True, layout=layout)
 
 
-janela1, janela2 = janela_login(), None
+janela1, janela2, janela3, janela4 = janela_login(), None, None, None
 
-while True:
 
-    window, event, values = sg.read_all_windows()
+def verifica_login():
 
-    if event == sg.WINDOW_CLOSED:
-        break
+    win = False
 
-    if event == 'Entrar':
+    if window == janela1:
+        try:
+            users[value['login']]
+        except KeyError:
+            window['text1'].update('Usuário não encontrado')
+        else:
+            window['text1'].update('')
 
-        if values['login'] in users:
-
-            if values['senha'] == users[values['login']]:
-
-                print('\033[32m-' * 60)
-                print('Acesso autorizado')
-                print('-' * 60)
-                break
+            if users[value['login']] != value['password']:
+                window['text2'].update('Senha incorreta')
 
             else:
+                window['text2'].update('')
 
-                print('\033[31m-' * 60)
-                print('Acesso negado (Senha incorreta)')
-                print('-' * 60)
+                if event == 'Entrar':
+                    win = True
+    return win
+
+
+def verifica_cadastro():
+
+    win = False
+
+    if window == janela2:
+        if value['login_cadastro'] in users:
+            window['text3'].update('Login já em uso')
 
         else:
+            window['text3'].update('')
 
-            print('\033[31m-' * 60)
-            print(f'Nenhuma conta registrada com "{values["login"]}"')
-            print('-' * 60)
+            if value['password_cadastro_1'] != value['password_cadastro_2']:
+                window['text4'].update('As senhas não coincidem')
+
+            else:
+                window['text3'].update('')
+
+                
+                if event == 'Cadastrar':
+                    red = open('Banco_de_dados.py', 'a')
+                    red.write(f'login = "{value["login_cadastro"]}"\n'
+                              f'senha = "{value["password_cadastro_1"]}"\n'
+                              'users.update({login: senha})\n')
+                win = True
+                
+    return win
+
+
+while True:
+    window, event, value = Sg.read_all_windows()
+
+    if event == Sg.WINDOW_CLOSED:
+        break
+
+    if window == janela1:
+        verifica_login()
 
     if event == 'Cadastro':
-
         janela1.hide()
         janela2 = janela_cadastro()
 
-    if event == 'cnv':
+    if window == janela2:
+        verifica_cadastro()
 
-        if values['login'] not in users:
-
-            print('\033[32m-' * 60)
-            print('Conta criada com Sucesso!!')
-            print('-' * 60)
-
-            red = open('../Banco_de_dados.py', 'a')
-            red.write(f'login = "{values["login"]}"\n'
-                      f'senha = "{values["senha"]}"\n'
-                      'users.update({login: senha})\n')
-
-            red.close()
-            break
-
-        else:
-
-            print('\033[31m-' * 60)
-            print('Usuário já cadastrado')
-            print('-' * 60)
-
-    if event == 'voltar':
+    if verifica_cadastro() is True:
         janela2.hide()
-        janela1.un_hide()
+        janela4 = janela_cadastro_correct()
+
+        time.sleep(5)
+        break
+
+    if verifica_login() is True:
+        janela1.hide()
+        janela3 = janela_login_correct()
+
+        time.sleep(5)
+        break
 
